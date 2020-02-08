c4a00988a51aa41f668e1fcff8dae262ce418c29
AnnaGW <a.bogoslovskaja@yandex.ru>
2020-02-08 22:42:25 +0500

    Учебный проект: одеть Надежду (вар 2)

--- js/setup.js
+++ js/setup.js
@@ -34,13 +34,7 @@ var COAT_COLORS = [
 ];
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 
-var FIREBALL_COLORS = [
-'#ee4830',
-'#30a8ee',
-'#5ce6c0',
-'#e848d5',
-'#e6e848'
-];
+var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 
 var selectRandomFromArray = function(array) {
   var randomIndex = Math.floor(Math.random() * array.length);
@@ -50,51 +44,52 @@ var selectRandomFromArray = function(array) {
 var setup = document.querySelector('.setup');
 var setupOpen = document.querySelector('.setup-open');
 var setupClose = setup.querySelector('.setup-close');
+var setupOpenIcon = setupOpen.querySelector('.setup-open-icon');
 
-var onPopupEscPress = function (evt) {
+var onPopupEscPress = function(evt) {
   if (evt.key === ESC_KEY) {
     closePopup();
   }
 };
 
-var openPopup = function () {
+var openPopup = function() {
   setup.classList.remove('hidden');
   document.addEventListener('keydown', onPopupEscPress);
 };
 
-var closePopup = function () {
+var closePopup = function() {
   setup.classList.add('hidden');
   document.removeEventListener('keydown', onPopupEscPress);
 };
 
-setupOpen.addEventListener('click', function () {
-  openPopup();
-});
+setupOpen.addEventListener('click', openPopup);
 
-setupClose.addEventListener('click', function () {
-  closePopup();
-});
+setupClose.addEventListener('click', closePopup);
 
-setupOpen.addEventListener('keydown', function (evt) {
-  if (evt.key === ENTER_KEY) {
+document.addEventListener('keydown', function(evt) {
+  if (evt.key === ENTER_KEY && evt.target === setupOpenIcon) {
     openPopup();
   }
 });
 
-setupClose.addEventListener('keydown', function (evt) {
+setupClose.addEventListener('keydown', function(evt) {
   if (evt.key === ENTER_KEY) {
-    setup.classList.add('hidden');
+    closePopup();
   }
 });
 
 var setupWizardForm = document.querySelector('.setup-wizard-form');
 var userNameInput = setupWizardForm.querySelector('.setup-user-name');
 
-userNameInput.addEventListener('invalid', function (evt) {
+userNameInput.addEventListener('invalid', function(evt) {
   if (userNameInput.validity.tooShort) {
-    userNameInput.setCustomValidity('Имя должно состоять не менее, чем из 2-х символов');
+    userNameInput.setCustomValidity(
+      'Имя должно состоять не менее, чем из 2-х символов'
+    );
   } else if (userNameInput.validity.tooLong) {
-    userNameInput.setCustomValidity('Имя должно состоять не более, чем из 25-ти символов');
+    userNameInput.setCustomValidity(
+      'Имя должно состоять не более, чем из 25-ти символов'
+    );
   } else if (userNameInput.validity.valueMissing) {
     userNameInput.setCustomValidity('Обязательное поле');
   } else {
@@ -111,27 +106,35 @@ var userWizardEyesInputValue = userWizard.querySelector('#eyes-color');
 var userWizardFireball = userWizard.querySelector('.setup-fireball-wrap');
 var userWizardFireballInputValue = userWizard.querySelector('#fireball-color');
 
-var ChangeColorOnClick = function (element, elementInputValue, colorArray) {
+var ChangeColorOnClick = function(element, elementInputValue, colorArray) {
   element.style.fill = selectRandomFromArray(colorArray);
   elementInputValue.value = element.style.fill;
 };
 
-var ChangeColorFireballOnClick = function (element, elementInputValue, colorArray) {
+var ChangeColorFireballOnClick = function(
+  element,
+  elementInputValue,
+  colorArray
+) {
   var fireballColor = selectRandomFromArray(colorArray);
   element.style.backgroundColor = fireballColor;
   elementInputValue.value = fireballColor;
 };
 
-userWizardCoat.addEventListener('click', function () {
+userWizardCoat.addEventListener('click', function() {
   ChangeColorOnClick(userWizardCoat, userWizardCoatInputValue, COAT_COLORS);
 });
 
-userWizardEyes.addEventListener('click', function () {
+userWizardEyes.addEventListener('click', function() {
   ChangeColorOnClick(userWizardEyes, userWizardEyesInputValue, EYES_COLORS);
 });
 
-userWizardFireball.addEventListener('click', function () {
-  ChangeColorFireballOnClick(userWizardFireball, userWizardFireballInputValue, FIREBALL_COLORS);
+userWizardFireball.addEventListener('click', function() {
+  ChangeColorFireballOnClick(
+    userWizardFireball,
+    userWizardFireballInputValue,
+    FIREBALL_COLORS
+  );
 });
 
 var createObject = function() {
